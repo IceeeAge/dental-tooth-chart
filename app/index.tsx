e249090869bbc1bffd59168941a1e65014c0c82e
IceeeAge <izmealsjh@gmail.com>
2025-02-01 20:05:46 +0800

    push

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import ToothChart from "@/components/ToothChart";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+       flex: 1,
+        
+      }}
+    >
+      <ToothChart />
+    </View>
+  );
+}
